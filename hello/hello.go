3bd8ea11343864b1e0e3b72ec3adcd03a32a1bd6
Bruno Torres <brunorctorres@gmail.com>
2025-12-17 09:31:09 -0400

    feat: adiciona tratamento de erro no uso do greetings

--- hello/hello.go
+++ hello/hello.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
 
 	"example.com/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
 	// Print a quote from rsc.io/quote module.
 	fmt.Println(quote.Go())
 
-	// Get a greeting message and print it from local example.com/greetings module.
-	message := greetings.Hello("Bruno")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
